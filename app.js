b0918f7c4f2401e98521fc7f70289bedd5140043
telgar <telgar@sportingindex.com>
2017-08-26 11:21:15 +0100

    Disabled 2nd soil sensor

--- app.js
+++ app.js
@@ -18,10 +18,12 @@ const soild1Db = low(constants.SOIL1_DB_PATH, {
 })
 soild1Db.defaults({ soil: [] }).write()
 
+/*
 const soild2Db = low(constants.SOIL2_DB_PATH, {
   storage: fileAsync
 })
 soild2Db.defaults({ soil: [] }).write()
+*/
 
 const board = new five.Board({
   repl: false
@@ -76,6 +78,7 @@ board.on("ready", function() {
   });
 
   // Soil moisture 2
+  /*
   var soil2Humidity = new five.Sensor({
     pin: constants.SOIL2_SENSOR_PIN,
     freq: constants.SAMPLE_RATE
@@ -95,7 +98,7 @@ board.on("ready", function() {
       .remove((o) => new Date(o.timestamp).getTime() < oldestRecord)
       .write()
   });
-
+  */
   trigger.init();
   trigger.start();
 });
--- formatters.js
+++ formatters.js
@@ -1,19 +1,17 @@
+const logger = require('./logger');
+
 // calculate the % soil humidity from raw readings
 var humidityPerc = function(reading, dryLimit, wetLimit, rangeLimit) {
   
   if (reading > dryLimit) { 
-    console.log('Dryer than dry! Reading = ' + reading)
+    logger.log('Formatters: Dryer than dry! Reading = ' + reading)
   }
 
   if (reading < wetLimit) {
-    console.log('Wetter than wet! Reading = ' + reading)
+    logger.log('Formatters: Wetter than wet! Reading = ' + reading)
   }
 
-  var percMoisture = (dryLimit - reading) / rangeLimit * 100;
-
-  //console.log("Raw reading: " + reading + " dry: " + dryLimit + " wet: " + wetLimit + " range: " + rangeLimit + " perc: " + percMoisture);
-  
-  return percMoisture;
+  return (dryLimit - reading) / rangeLimit * 100;;
 }
 
 module.exports = {
