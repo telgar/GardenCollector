9eac013ec5c5b8d75ab9c344f5667d23085624c0
telgar <telgar@sportingindex.com>
2017-11-07 07:44:02 +0000

    changed logging

--- logger.js
+++ logger.js
@@ -10,15 +10,17 @@ logDb.defaults({ logs: [] }).write()
 
 const log = function() {
 
-    for (let i = 0; i < arguments.length; i++) {
-        
-    }
     var args = [].slice.call(arguments);
-    console.log(args);
-
+    
     logDb.get('logs')
         .push({ timestamp: new Date(), log: args })
         .write()
+
+    let oldestRecord = new Date().getTime() - constants.LOG_LIMIT
+    
+    logDb.get('logs')
+      .remove((o) => new Date(o.timestamp).getTime() < oldestRecord)
+      .write()
 }
 
 module.exports = {
